af214ad1871b1550c452e95b995441b3f35d6d83
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-03-31 20:26:19 +0200

    Poprawki.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,21 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         Person person1 = new Person("Kamil", "IT");
-        Tasks tasks1 = new Tasks("Sprzątanie", "posprzątać kuchnie", 0, person1);
-        System.out.println("Nazwa zadania: " + tasks1.getTaskName() + " - " + tasks1.getTaskDescription() + "\nPriorytet: "
-                + tasks1.getPriority() + " " + "\nOsoba: " + tasks1.getPerson().getFirstName() + " " + tasks1.getPerson().getLastName());
-        System.out.println("Mały prioytet? " + tasks1.lowPriority(tasks1.getPriority()));
-        System.out.println("Średni prioytet? " + tasks1.mediumPriority(tasks1.getPriority()));
-        System.out.println("Duży prioytet? " + tasks1.highPriority(tasks1.getPriority()));
+        Task task1 = new Task("Sprzątanie", "posprzątać kuchnie", 0, person1);
+        System.out.println("Nazwa zadania: " + task1.getName() + " - " + task1.getDescription() + "\nPriorytet: "
+                + task1.getPriority() + " " + "\nOsoba: " + task1.getPerson().getFirstName() + " " + task1.getPerson().getLastName());
+        System.out.println("Mały prioytet? " + task1.lowPriority(task1.getPriority()));
+        System.out.println("Średni prioytet? " + task1.mediumPriority(task1.getPriority()));
+        System.out.println("Duży prioytet? " + task1.highPriority(task1.getPriority()));
 
         System.out.print("\n");
 
         Person person2 = new Person("Jacek", "Kracek");
-        Tasks tasks2 = new Tasks("Umyć okna", "umyć okna w sypialni", -1, person2);
-        System.out.println("Nazwa zadania: " + tasks2.getTaskName() + " - " + tasks2.getTaskDescription() + "\nPriorytet: "
-                + tasks2.getPriority() + " " + "\nOsoba: " + tasks2.getPerson().getFirstName() + " " + tasks2.getPerson().getLastName());
-        System.out.println("Mały prioytet? " + tasks2.lowPriority(tasks2.getPriority()));
-        System.out.println("Średni prioytet? " + tasks2.mediumPriority(tasks2.getPriority()));
-        System.out.println("Duży prioytet? " + tasks2.highPriority(tasks2.getPriority()));
+        Task task2 = new Task("Umyć okna", "umyć okna w sypialni", -1, person2);
+        System.out.println("Nazwa zadania: " + task2.getName() + " - " + task2.getDescription() + "\nPriorytet: "
+                + task2.getPriority() + " " + "\nOsoba: " + task2.getPerson().getFirstName() + " " + task2.getPerson().getLastName());
+        System.out.println("Mały prioytet? " + task2.lowPriority(task2.getPriority()));
+        System.out.println("Średni prioytet? " + task2.mediumPriority(task2.getPriority()));
+        System.out.println("Duży prioytet? " + task2.highPriority(task2.getPriority()));
     }
 }
